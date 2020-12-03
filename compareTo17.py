5fc7f633f389233f99e11e1f5f305f3e192be7cb
dokajajob <dokajajob@gmail.com>
2020-12-03 14:39:49 +0200

    new

--- compareTo17.py
+++ compareTo17.py
@@ -1,14 +1,18 @@
-def dif():
+def dif(n):
     """compares number to 17"""
     num = 17
-    inum = input("Enter num ")
-    res = int(inum) - (num)
-    print(res)
+    #inum = input("Enter num ")
+    res = int(n) - num
+    print("dif is: ", res)
 
     if res >= 0:
         res = res*2
         print("dif is doubled = ", res)
         #return res
     else:
-        print("the number is less then 17")
-dif()
+        print("the numb®er is less then 17 is: ", n)
+#dif(n)
+
+inum = input("enter number ")
+#int(inum)
+dif(inum)
