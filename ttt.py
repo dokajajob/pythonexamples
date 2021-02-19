8042b734620c4504a0a7f53fce09d21cdeecd945
dokajajob <dokajajob@gmail.com>
2021-02-19 16:30:46 +0200

    Scripted auto-commit on change (2021-02-19 16:30:45) by gitwatch.sh

--- ttt.py
+++ ttt.py
@@ -1,2 +1,2 @@
 import os
-print([f for f in os.listdir('/home/dokaja/ed/html_css_js_tutorial')])
+print([f for f in os.listdir('/home/dokaja/ed/html_css_js_tutorial') ]) if os.path.isfile(os.path.join('/home/students', f))
