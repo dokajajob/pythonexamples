b4000d442f486a20701a5cb8db4bd2f509f57d9e
dokajajob <dokajajob@gmail.com>
2021-02-19 16:28:17 +0200

    Scripted auto-commit on change (2021-02-19 16:28:17) by gitwatch.sh

--- ttt.py
+++ ttt.py
@@ -1,7 +1,2 @@
-var_list = ['a', 'b', 'c']
-x, y, z = (var_list + [None] * 3)[:3]
-print(x, y, z)
-var_list = [100, 20.25]
-x, y = (var_list + [None] * 2)[:2]
-print(x, y)
-print((var_list))
+import os
+print([f for f in os.listdir('/home/')])
