d299d6eb2cecabeea050276b0203a1a803c8ce78
dokajajob <dokajajob@gmail.com>
2021-02-19 14:49:33 +0200

    autocommit on change

--- python.py
+++ python.py
@@ -245,4 +245,8 @@ print("Float value information: ",sys.float_info)
 print("\nInteger value information: ",sys.int_info)
 print("\nMaximum size of an integer: ",sys.maxsize) 
 
-27)
+27) sum of all counts in a collections.
+
+import collections
+num = [2,2,4,6,6,8,6,10,4]
+print(sum(collections.Counter(num).values()))
