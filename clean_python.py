3eeb2d4d95fce529cb7593c90896391854b2c6a6
dokajajob <dokajajob@gmail.com>
2021-02-22 20:10:35 +0200

    Scripted auto-commit on change (2021-02-22 20:10:35) by gitwatch.sh

--- clean_python.py
+++ clean_python.py
@@ -1,4 +1,4 @@
-f = open("python.py", "r,w")
+f = open("python.py", "r+w")
 #output = open("python_last.py", "w")
 
 f.write(f.read().replace("ads via Carbon","").replace("No code required, set up in minutes! Flatfile Concierge turns data onboarding into a collaborative experience.","").replace("ADS VIA CARBON",""))
