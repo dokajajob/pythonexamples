7b43566d3cb2b35f023e8b8180df8f2a1859209b
dokajajob <dokajajob@gmail.com>
2021-03-01 14:40:18 +0200

    Scripted auto-commit on change (2021-03-01 14:40:18) by gitwatch.sh

--- python_last.py
+++ python_last.py
@@ -2241,3 +2241,34 @@ Note: The popitem() method raises a KeyError error if the dictionary is empty.
 aaaaa
 =======
 >>>>>>> d7d5a84feba5209699a443338c747b96ad454c45
+
+82) super()
+
+https://www.youtube.com/watch?v=aBexJgZ6GjI&ab_channel=ProgrammingKnowledge
+
+https://rhettinger.wordpress.com/2011/05/26/super-considered-super/
+
+83) Similarly, we can overload other operators as well. The special function that we need to implement is tabulated below.
+
+Operator  Expression  Internally
+Addition  p1 + p2 p1.__add__(p2)
+Subtraction p1 - p2 p1.__sub__(p2)
+Multiplication  p1 * p2 p1.__mul__(p2)
+Power p1 ** p2  p1.__pow__(p2)
+Division  p1 / p2 p1.__truediv__(p2)
+Floor Division  p1 // p2  p1.__floordiv__(p2)
+Remainder (modulo)  p1 % p2 p1.__mod__(p2)
+Bitwise Left Shift  p1 << p2  p1.__lshift__(p2)
+Bitwise Right Shift p1 >> p2  p1.__rshift__(p2)
+Bitwise AND p1 & p2 p1.__and__(p2)
+Bitwise OR  p1 | p2 p1.__or__(p2)
+Bitwise XOR p1 ^ p2 p1.__xor__(p2)
+Bitwise NOT ~p1 p1.__invert__()
+
+Operator  Expression  Internally
+Less than p1 < p2 p1.__lt__(p2)
+Less than or equal to p1 <= p2  p1.__le__(p2)
+Equal to  p1 == p2  p1.__eq__(p2)
+Not equal to  p1 != p2  p1.__ne__(p2)
+Greater than  p1 > p2 p1.__gt__(p2)
+Greater than or equal to  p1 >= p2  p1.__ge__(p2)
